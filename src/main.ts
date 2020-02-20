100c2cedc8fa0e509b6b88100c088d1918d002b4
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-02-20 07:52:15 -0800

    Add multiline flag to regular expression so the anchors work

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function run(): Promise<void> {
       const mixText = fs
         .readFileSync(mixPath)
         .toString()
-        .replace(/^(\s+)version: "([^"]+)",$/, `$1version: "$2+${hash}"`)
+        .replace(/^(\s+)version: "([^"]+)",$/m, `$1version: "$2+${hash}"`)
 
       core.debug(`vvvvv ${mixPath} vvvvv`)
       core.debug(mixText)
